d3e946646aeff16758295d30101d94a5e5ec36d1
Mathias Monnerville <monnerville@iap.fr>
2009-01-05 13:16:59 +0000

    Code refactoring Code fully documented

--- terapix/media/js/shoppingcart.js
+++ terapix/media/js/shoppingcart.js
@@ -1,12 +1,14 @@
-
 /*
  * Class: ShoppingCart
  * Basic shopping cart implementation
  *
- * Note:
+ * File:
  *
- * Please note that this page documents Javascript code. <ShoppingCart> is a pseudo-class, 
- * it provides encapsulation and basic public/private features.
+ *  shoppingcart.js
+ *
+ * Dependencies:
+ *
+ *  scritptaculous.js
  *
  * Constructor Parameters:
  *
@@ -16,37 +18,67 @@
  */
 function ShoppingCart(container_id, varName)
 {
-	// Name of instance if global namespace
-	var instance_name = varName;
-	var container = document.getElementById(container_id);
+	// Group: Constants
+	// -----------------------------------------------------------------------------------------------------------------------------
 
+
+	/*
+	 * Var: _instance_name
+	 * Name of instance in global namespace
+	 *
+	 */
+	var _instance_name = varName;
+	/*
+	 * Var: _container
+	 * Parent DOM container
+	 *
+	 */ 
+	var _container = document.getElementById(container_id);
+	/*
+	 * Var: _itemsCount
+	 * Number of items in cart
+	 *
+	 */ 
 	var itemsCount = 0;
-	var itemCaption = ['item', 'items'];
 
-	function getArticlesCountMsg() {
-		return itemsCount ? itemsCount + ' ' + 
-			(itemsCount > 1 ? itemCaption[1] : itemCaption[0]) + ' to process' : 'Empty cart';
+
+	// Group: Functions
+	// -----------------------------------------------------------------------------------------------------------------------------
+
+
+	/*
+	 * Function: _getArticlesCountMsg
+	 * Cart text rendering
+	 *
+	 */ 
+	function _getArticlesCountMsg() {
+		return itemsCount ? itemsCount + ' item' + 
+			(itemsCount > 1 ? 's' : '') + ' to process' : 'Empty cart';
 	}
 
-	function render() {
+	/*
+	 * Function: _render
+	 * Renders shopping cart
+	 *
+	 */ 
+	function _render() {
 		var xhr = new HttpRequest(
 			null,
 			// Use default error handler
 			null,
 			// Custom handler for results
 			function(resp) {
-				itemsCount = resp['count'];
-				var div = document.createElement('div');
-				div.setAttribute('id', 'shoppingCart');
-				div.setAttribute('class', 'nonempty');
-				div.setAttribute('onclick', 'location.href="/youpi/cart/";');
+				itemsCount = resp.count;
+				var div = Builder.node('div', { id: 'shoppingCart',
+												className: 'nonempty',
+												onclick: "location.href='/youpi/cart/'" }, [
+								Builder.node('img', { src: '/media/themes/' + guistyle + '/img/misc/minicart.gif' }),
+								_getArticlesCountMsg()
+						  ]);
 
-				var img = document.createElement('img');
-				img.setAttribute('src', '/media/themes/' + guistyle + '/img/misc/minicart.gif');
-				div.appendChild(img);
-				div.appendChild(document.createTextNode(getArticlesCountMsg()));
-				removeAllChildrenNodes(container);
-				container.appendChild(div);
+				// Clear contents
+				_container.update();
+				_container.appendChild(div);
 			}
 		);
 
@@ -54,60 +86,51 @@ function ShoppingCart(container_id, varName)
 		xhr.send('/youpi/cart/itemsCount/');
 	}
 
-	// Refresh display
+	/*
+	 * Function: refresh
+	 * Updates cart rendering
+	 *
+	 */ 
 	this.refresh = function() {
-		render();
-	}
-
-	function showToolTip() {}
-
-	function removeAllChildrenNodes(node)
-	{
-		// Element ?
-		if (node.nodeType != 1)
-			return;
-	
-		if (node.hasChildNodes()) {
-			for (var n=0; n < node.childNodes.length; n++) {
-				node.removeChild(node.childNodes[n]);
-			}
-		}
+		_render();
 	}
 
 	/*
-	 * Adds an item to the cart
+	 * Function: addProcessing
+	 * Adds an item to the shopping cart
 	 *
-	 */
-	// handler: called when processing added
-	function addItem(json, handler) {
+	 * Note:
+	 * The *obj* object must have the following properties.
+	 *
+	 *  plugin_name - string: internal plugin name
+	 *  userData - object: custom user data 
+	 *
+	 * Paremeters:
+	 *  obj - object: arbitrary object
+	 *  handler - function: custom function to execute after an item has been added
+	 *
+	 */ 
+	this.addProcessing = function(obj, handler) {
 		var xhr = new HttpRequest(
-			container.id,
-			// Use default error handler
+			null,
 			null,
 			// Custom handler for results
 			function(resp) {
-				var nb = resp['data'].length;
-				render();
-				// Call custom handler
-				if (handler) handler();
-			}
-		);
+				var r = new HttpRequest(
+					null,
+					null,
+					function(resp) {
+						var nb = resp['data'].length;
+						_render();
+						// Call custom handler
+						if (handler) handler();
+					}
+				);
 
-		var post = 'plugin=' + json['plugin_name'] + '&userData=' + escape(json['userData']);
-		// Check for cookie
-		xhr.send('/youpi/cart/additem/', post);
-	}
-
-	// json: { 'plugin_name' : 'name', 'userData' : data }
-	// handler: called when processing added
-	this.addProcessing = function(json, handler) {
-		var xhr = new HttpRequest(
-			container.id,
-			// Use default error handler
-			null,
-			// Custom handler for results
-			function(resp) {
-				addItem(json, handler);
+				var post = 	'plugin=' + obj.plugin_name + 
+							'&userData=' + escape(Object.toJSON(obj.userData));
+				// Check for cookie
+				r.send('/youpi/cart/additem/', post);
 			}
 		);
 
@@ -116,54 +139,62 @@ function ShoppingCart(container_id, varName)
 	}
 
 	/*
-	 * Deletes an item in the cart (for a plugin)
+	 * Function: deletePluginItem
+	 * Deletes an item from the shopping cart
 	 *
-	 */
-	// json: { 'plugin_name' : 'name', 'idx' : index of row in plugin context }
-	this.deletePluginItem = function(json, handlerAfterDelete) {
-		// Custom handler called upon successful deletion
-		var chandler = handlerAfterDelete ? handlerAfterDelete : null;
+	 * Note:
+	 * The *obj* object must have the following properties.
+	 *
+	 *  plugin_name - string: internal plugin name
+	 *  idx - int: row index (in plugin context)
+	 *
+	 * Paremeters:
+	 *  obj - object: arbitrary object
+	 *  deleteAll - boolean: true to delete all plugin's items at once
+	 *  handler - function: custom function to execute after an item has been deleted
+	 *
+	 */ 
+	this.deletePluginItem = function(obj, deleteAll, handler) {
+		var handler = typeof handler == 'function' ? handler : null;
+		var deleteAll = typeof deleteAll == 'boolean' ? deleteAll : false;
+
 		var xhr = new HttpRequest(
-			container.id,
+			null,
 			// Use default error handler
 			null,
 			// Custom handler for results
 			function(resp) {
-				var nb = resp['data'].length;
-				render();
-				if (chandler) chandler();
+				var nb = resp.data.length;
+				_render();
+				if (handler) handler();
 			}
 		);
 
-		var post = 'plugin=' + json['plugin_name'] + '&idx=' + json['idx'];
-		// Check for cookie
+		var post = 'plugin=' + obj.plugin_name;
+		if (!deleteAll)
+			post += '&idx=' + obj.idx;
 		xhr.send('/youpi/cart/delitem/', post);
 	}
 
 	/*
-	 * Deletes all items of one plugin in the cart
+	 * Function: deleteAllPluginItem
+	 * Deletes an item from the shopping cart
 	 *
-	 */
-	this.deleteAllPluginItems = function(plugin_name, handlerAfterDelete) {
-		// Custom handler called upon successful deletion
-		var chandler = handlerAfterDelete ? handlerAfterDelete : null;
-		var xhr = new HttpRequest(
-			container.id,
-			// Use default error handler
-			null,
-			// Custom handler for results
-			function(resp) {
-				var nb = resp['data'].length;
-				render();
-				if (chandler) chandler();
-			}
-		);
-
-		var post = 'plugin=' + plugin_name;
-		// Check for cookie
-		xhr.send('/youpi/cart/delitem/', post);
+	 * Note:
+	 * The *obj* object must have the following properties.
+	 *
+	 *  plugin_name - string: internal plugin name
+	 *  idx - int: row index (in plugin context)
+	 *
+	 * Paremeters:
+	 *  obj - object: arbitrary object
+	 *  handler - function: custom function to execute after an item has been deleted
+	 *
+	 */ 
+	this.deleteAllPluginItems = function(plugin_name, handler) {
+		this.deletePluginItem(plugin_name, true, handler);
 	}
 
 	// Main entry point
-	render();
+	_render();
 }
