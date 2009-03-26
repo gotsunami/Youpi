9a22133d943a18cbce97977151ad64822713df86
Mathias Monnerville <monnerville@iap.fr>
2009-03-26 12:04:58 +0000

    Default processing kind are now checked by the script.

--- terapix/script/checksetup.py
+++ terapix/script/checksetup.py
@@ -1,25 +1,53 @@
 
 import sys, os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'terapix.settings'
+if '..' not in sys.path:
+	sys.path.insert(0, '..')
+
+try:
+	from django.contrib.auth.models import User
+	from django.db.models import Q
+	from django.db import IntegrityError
+	#
+	from terapix.youpi.models import *
+	from terapix.youpi.pluginmanager import PluginManager
+except ImportError:
+	print 'Please run this command from the terapix subdirectory.'
+	sys.exit(1)
+
 
 YOUPI_USER = 'youpiadm'
 
-def log(group, msg):
-	"""
-	Prints a formatted log message to stdout.
-	@param group string group name
-	@param msg string message to display
-	"""
-	print "[%s] %s" % (group, msg)
+class LoggerError(Exception): pass
 
-def sub(msg):
-	print " * %s" % msg
+class Logger:
+	def __init__(self):
+		self.group = None
+		self.title = None
+		self.indent = 3
+
+	def setGroup(self, group, title):
+		"""
+		Prints a formatted log message to stdout.
+		@param group string group name
+		@param title string group's title
+		"""
+		self.group = group
+		self.title = title
+		print "[%s] %s" % (group, title)
+
+	def log(self, msg):
+		if self.group:
+			print "%s%s" % (' ' * self.indent, msg)
+		else:
+			print msg
 
 def setup_db():
 	"""
 	Database setup.
 	"""
 
-	log('init', 'setup database with mandatoy data')
+	logger.setGroup('init', 'Setup database with mandatoy data')
 
 	#surveys = Survey.objects.all()
 	#if surveys:
@@ -27,7 +55,7 @@ def setup_db():
 #		instrus = Instrument.objects.filter(Q(name = 'MEGACAM') | Q(name = 'WIRCAM'))
 
 	# Default first quality evaluation comments
-	sub('Adding default first quality evaluation comments')
+	logger.log('Adding default first quality evaluation comments')
 	comments = ('---', 'Poor seeing (>1.2)', 'Astrometric keyword problem: some CCDs off',
 				'D3-i rejection', 'Galaxy counts below/above expectations', 'Some CCDs or half-CCDs missing',
 				'Defocus?', 'PSF/Object image doubled', 'Diffuse light contamination', 'Unusual PSF anisotropy pattern',
@@ -37,21 +65,32 @@ def setup_db():
 		try:
 			c = FirstQComment(comment = com)
 			c.save()
-		except:
+			logger.log("Added comment: '%s'" % (com))
+		except IntegrityError:
 			# Already in DB
 			pass
 
 	# Default processing kinds
+	logger.log('Adding default processing kinds')
+	manager = PluginManager()
 
-	print 'OK'
-
+	for plugin in manager.plugins:
+		try:
+			kind = Processing_kind(name = plugin.id, label = plugin.optionLabel)
+			kind.save()
+			logger.log('Added ' + plugin.id)
+		except IntegrityError:
+			# Already existing
+			pass
 
 def setup_users():
-	print "Check if there is at least one user...",
+	logger.setGroup('users', 'Check if there is at least one user')
+
 	users = User.objects.all().order_by('username')
 	if users:
-		print "OK (%d)" % len(users)
+		logger.log("Found %d users" % len(users))
 	else:
+		logger.log("No user found, adding a default one (%s)" % YOUPI_USER)
 		# Add at least one user
 		user = User.objects.create_user(username = YOUPI_USER, password = YOUPI_USER)
 		user.is_staff = False
@@ -59,16 +98,18 @@ def setup_users():
 		user.save()
 
 def setup_policies():
-	print "Adding defaut ALL condor policy...",
+	logger.setGroup('policies', 'Checking for a defaut ALL condor policy')
 	pols = CondorNodeSel.objects.filter(label = 'ALL', is_policy = True)
 	if not len(pols):
+		logger.log("Not found, adding an ALL policy")
 		# Not existing
 		pol = CondorNodeSel(label = 'ALL', user = User.objects.all()[0], is_policy = True)
 		# Select all nodes matching at least 1 Mb of RAM (so this define a policy which selects all
 		# cluster nodes
 		pol.nodeselection = 'MEM,G,1,M'
 		pol.save()
-	print "OK"	
+	else:
+		logger.log("ALL policy found")
 
 def setup():
 	setup_db()
@@ -77,16 +118,5 @@ def setup():
 	
 
 if __name__ == '__main__':
-	os.environ['DJANGO_SETTINGS_MODULE'] = 'terapix.settings'
-	if '..' not in sys.path:
-		sys.path.insert(0, '..')
-
-	try:
-		from django.contrib.auth.models import User
-		from django.db.models import Q
-		from terapix.youpi.models import *
-	except ImportError:
-		print 'Please run this command from the terapix subdirectory.'
-		sys.exit(1)
-
+	logger = Logger()
 	setup()
