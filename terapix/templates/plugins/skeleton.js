f9851431b12e8b9c93c9685951c44833d45320d0
Mathias Monnerville <monnerville@iap.fr>
2009-03-31 13:12:14 +0000

    Added Copyright information and license to source files.

--- terapix/templates/plugins/skeleton.js
+++ terapix/templates/plugins/skeleton.js
@@ -1,3 +1,16 @@
+/*****************************************************************************
+ *
+ * Copyright (c) 2008-2009 Terapix Youpi development team. All Rights Reserved.
+ *                    Mathias Monnerville <monnerville@iap.fr>
+ *                    Gregory Semah <semah@iap.fr>
+ *
+ * This program is Free Software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ *****************************************************************************/
+
 /*
  * Group: Skeleton Plugin
  *
--- terapix/youpi/context_processors.py
+++ terapix/youpi/context_processors.py
@@ -1,3 +1,16 @@
+##############################################################################
+#
+# Copyright (c) 2008-2009 Terapix Youpi development team. All Rights Reserved.
+#                    Mathias Monnerville <monnerville@iap.fr>
+#                    Gregory Semah <semah@iap.fr>
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+##############################################################################
+
 """
 A set of custom Youpi request processors that return dictionaries to be merged into a
 template context. Each function takes the request object as its only parameter
