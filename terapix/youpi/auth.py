0512076804ca4eed746288e70509286f05535e86
Mathias Monnerville <monnerville@iap.fr>
2009-05-05 16:51:23 +0200

    New auth module for Youpis' specific permissions management

---
+++ terapix/youpi/auth.py
@@ -0,0 +1,30 @@
+##############################################################################
+#
+# Copyright (c) 2008-2009 Terapix Youpi development team. All Rights Reserved.
+#                    Mathias Monnerville <monnerville@iap.fr>
+#                    Gregory Semah <semah@iap.fr>
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+##############################################################################
+
+"""
+Set of functions to deal with authentication within Youpi.
+"""
+
+class perms:
+	def can_read(self):
+		pass
+
+	def can_write(self):
+		pass
+
+def get_user_rights(user, model):
+	"""
+	Returns context variables for Youpi's release system (Application menu).
+	"""
+
+	pass
