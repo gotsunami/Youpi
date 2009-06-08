6570cf19145e5aacf8485602c4ffe7fabcc34a27
Mathias Monnerville <monnerville@iap.fr>
2009-06-08 13:46:23 +0200

    Swarp plugin uses CMD_SWARP

--- terapix/youpi/plugins/swarp.py
+++ terapix/youpi/plugins/swarp.py
@@ -280,10 +280,11 @@ notify_user             = monnerville@iap.fr
 
 
 		condor_submit_entry = """
-arguments               = %(encuserdata)s /usr/local/bin/condor_transfert.pl /usr/bin/swarp %(params)s @%(imgsfile)s -c %(config)s 2>/dev/null
+arguments               = %(encuserdata)s /usr/local/bin/condor_transfert.pl %(swarp)s %(params)s @%(imgsfile)s -c %(config)s 2>/dev/null
 # YOUPI_USER_DATA = %(userdata)s
 environment             = USERNAME=%(user)s; TPX_CONDOR_UPLOAD_URL=%(tpxupload)s; PATH=/usr/local/bin:/usr/bin:/bin:/opt/bin:/opt/condor/bin; YOUPI_USER_DATA=%(encuserdata)s
 queue""" %  {	'encuserdata' 	: encUserData, 
+				'swarp'			: CMD_SWARP,
 				'params'		: swarp_params,
 				'config'		: os.path.basename(customrc),
 				'userdata'		: userData, 
