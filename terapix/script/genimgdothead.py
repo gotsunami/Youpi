4e560a4ae096b7a3d90db16041817bb4b824f288
Mathias Monnerville <monnerville@iap.fr>
2010-02-11 17:06:42 +0100

    Dot head gen script: removed all Django deps; use MySQLdb instead
    Since this script has to be used by the ingestion script on any
    cluster node (thus without any Django installation), all dependencies
    to Django had to be removed.

--- terapix/script/genimgdothead.py
+++ terapix/script/genimgdothead.py
@@ -14,15 +14,9 @@
 import sys, os, os.path, types
 import pyfits, re, datetime
 import marshal, zlib, base64
-os.environ['DJANGO_SETTINGS_MODULE'] = 'terapix.settings'
-if '..' not in sys.path:
-	sys.path.insert(0, '..')
-
-try:
-	from terapix.youpi.models import *
-except ImportError, e:
-	print "Please run this command from the terapix subdirectory: %s" % e
-	sys.exit(1)
+import MySQLdb
+from DBGeneric import *
+import settings
 
 def genImageDotHead(image_id):
 	"""
@@ -31,13 +25,31 @@ def genImageDotHead(image_id):
 	order to get a proper HDU data.
 	@return tuple of hdudata and total number of hdus in this image (primary + extansions)
 	"""
+	db = DB(host = settings.DATABASE_HOST,
+			user = settings.DATABASE_USER,
+			passwd = settings.DATABASE_PASSWORD,
+			db = settings.DATABASE_NAME)
+
+	# Beginning transaction
+	g = DBGeneric(db.con)
+
 	if type(image_id) != types.IntType:
 		raise ValueError, "first argument image_id must be an integer"
-	try: img = Image.objects.filter(id = image_id)[0]
-	except:
-		raise LookupError, "no image found with ID: %s" % image_id
 
-	fname = os.path.join(img.path, img.name + '.fits')
+	r = g.execute("""
+SELECT im.path, im.name, ru.name, ins.itt, ins.name, im.dateobs, cha.name, im.flat, 
+im.airmass, im.equinox, im.object, im.exptime, im.mask, im.alpha, im.delta
+FROM youpi_image AS im, youpi_instrument AS ins, youpi_channel AS cha, youpi_rel_ri AS ri, youpi_run AS ru
+WHERE im.id=%d 
+AND im.instrument_id=ins.id
+AND im.channel_id=cha.id
+AND ri.image_id = im.id
+AND ru.id = ri.run_id
+""" % image_id)
+	if not r: raise LookupError, "no image found with ID: %s" % image_id
+	r = r[0]
+
+	fname = os.path.join(r[0], r[1] + '.fits')
 	if not os.path.exists(fname):
 		fname = re.sub(r'_\d+\.fits$', '.fits', fname)
 
@@ -45,26 +57,26 @@ def genImageDotHead(image_id):
 	hdulist.close()
 
 	# Get instrument translation table
-	itt = marshal.loads(zlib.decompress(base64.decodestring(img.instrument.itt)))
+	itt = marshal.loads(zlib.decompress(base64.decodestring(r[3])))
 
-	try: run = Rel_ri.objects.filter(image = img)[0].run.name
+	try: run = r[2]
 	except: run = None
 
 	data = {
 		'YRUN'			: run, 
 #		'YDETECTOR': , 
 #		'YTELESCOP': , 
-		'YINSTRUMENT'	: img.instrument.name, 
-		'YDATEOBS'		: img.dateobs, 
-		'YFILTER'		: img.channel.name,
-		'YFLAT'			: img.flat,
-		'YAIRMASS'		: img.airmass,
-		'YEQUINOX'		: img.equinox,
-		'YOBJECT'		: img.object,
-		'YEXPTIME'		: img.exptime,
-		'YMASK'			: img.mask,
-		'YRA'			: img.alpha,
-		'YDEC'			: img.delta,
+		'YINSTRUMENT'	: r[4],
+		'YDATEOBS'		: r[5],
+		'YFILTER'		: r[6],
+		'YFLAT'			: r[7],
+		'YAIRMASS'		: r[8],
+		'YEQUINOX'		: r[9],
+		'YOBJECT'		: r[10],
+		'YEXPTIME'		: r[11],
+		'YMASK'			: r[12],
+		'YRA'			: r[13],
+		'YDEC'			: r[14],
 	}
 
 	hdudata = {}
@@ -103,7 +115,7 @@ def main():
 		print "Usage: %s image_db_id" % sys.argv[0]
 		sys.exit(1)
 
-	data, lenght = genImageDotHead(int(sys.argv[1]))
+	data, lenght, missing = genImageDotHead(int(sys.argv[1]))
 	if len(data):
 		for i in range(lenght):
 			for k, v in data.iteritems():
