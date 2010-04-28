c2bbd7f6eca03475e5d822cd78cce5bd6a02c0ef
Mathias Monnerville <monnerville@iap.fr>
2010-04-28 13:02:59 +0200

    More unit tests for plugin manager

--- terapix/unittesting/test_pluginmanager.py
+++ terapix/unittesting/test_pluginmanager.py
@@ -15,9 +15,10 @@
 Tests for the plugin manager
 """
 
-import unittest, types, sys
+import unittest, types, sys, time
 #
 from terapix.youpi.pluginmanager import PluginManager, ProcessingPlugin
+from django.http import HttpRequest
 
 class PluginManagerTest(unittest.TestCase):
 	"""
@@ -39,6 +40,63 @@ class PluginManagerTest(unittest.TestCase):
 	def test_getPluginByName(self):
 		for k in ({'k': 0}, lambda x: x, 1, object()):
 			self.assertRaises(TypeError, self.pm.getPluginByName, k)
+
+class ProcessingPluginTest(unittest.TestCase):
+	"""
+	Tests the ProcessingPlugin class
+	"""
+	def setUp(self):
+		self.plugin = ProcessingPlugin()
+
+	def test_getUniqueCondorJobId(self):
+		id = self.plugin.getUniqueCondorJobId()
+		self.assertEquals(type(id), types.StringType)
+		# Tests pseudo randomness
+		self.assertNotEquals(id, self.plugin.getUniqueCondorJobId())
+
+	def test_getConfigValue(self):
+		for k in ({'k': 0}, lambda x: x, 1, object()):
+			self.assertRaises(TypeError, self.plugin.getConfigValue, k, 'KEYWORD')
+			self.assertRaises(TypeError, self.plugin.getConfigValue, [], k)
+
+		content = ['nop nop nop', 'KEYWORD1 VALUE1']
+		w = self.plugin.getConfigValue(content, 'wrongkey')
+		v = self.plugin.getConfigValue(content, 'KEYWORD1')
+		self.assertEquals(type(w), types.BooleanType)
+		self.assertEquals(w, False)
+
+		self.assertEquals(type(v), types.StringType)
+		self.assertEquals(v, 'VALUE1')
+		self.assertEquals(self.plugin.getConfigValue([], 'key'), False)
+
+	def test_reports(self):
+		self.assertEquals(type(self.plugin.reports()), types.ListType)
+
+	def test_getConfigurationFilePath(self):
+		p = self.plugin.getConfigurationFilePath()
+		self.assertEquals(type(p), types.StringType)
+		# Tests pseudo randomness
+		time.sleep(.1)
+		self.assertNotEquals(p, self.plugin.getConfigurationFilePath())
+
+	def test_getUserResultsOutputDir(self):
+		req = HttpRequest()
+		req.user = type('User', (object,), {'username': 'user'})()
+
+		for k in ({'k': 0}, lambda x: x, 1, object()):
+			self.assertRaises(TypeError, self.plugin.getUserResultsOutputDir, k) 		# request
+			self.assertRaises(TypeError, self.plugin.getUserResultsOutputDir, req, k)	# oldPath
+			self.assertRaises(TypeError, self.plugin.getUserResultsOutputDir, req, 'path', k)	# oldUserName
+
+		self.assertRaises(TypeError, self.plugin.getUserResultsOutputDir, req, 'path')	# Missing oldUserName
+
+		res = self.plugin.getUserResultsOutputDir(req)
+		self.assertEquals(type(res), types.StringType)
+		res = self.plugin.getUserResultsOutputDir(req, '/my/path/olduser/', 'olduser')
+		self.assertEquals(res, '/my/path/user/')
+
+	def test_getConfigFileContent(self):
+		pass
 
 
 if __name__ == '__main__':
