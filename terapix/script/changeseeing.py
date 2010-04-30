a8e14a32f00d85c76541774ff57331382e7a314b
Mathias Monnerville <monnerville@iap.fr>
2010-04-30 15:44:49 +0200

    Bug 198: QFits: fix XML parsing of psfex, compute (and store) image seeing in arcsec
    The standalone changeseeing (to be renamed) script is working fine. It
    allows to reparse all successful qfits output data and reingest the results
    in the database, which may be useful.
    
    The wrapper processing script has been improved. When parsing output of psfex,
    any parsing error exception that may be raised in case of old psfex.xml data
    format will be catched and a call to the old parsing algorithm for psfex will
    be made instead (by calling the parse_psfex_old_xml() function).
    
    The changeseeing could be improved a little bit, since for the moment if forces
    to loop over all the successfull qfits. An option will be added to skip a qfits
    entry if some values are not NULL.

--- terapix/script/changeseeing.py
+++ terapix/script/changeseeing.py
@@ -20,13 +20,16 @@ import sys, os, string, curses, os.path
 import marshal, base64, types, re
 import datetime, time
 from optparse import OptionParser
+import xml.dom
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'terapix.settings'
 if '..' not in sys.path:
 	sys.path.insert(0, '..')
+	sys.path.insert(0, 'lib')
 
 try:
 	from decimal import getcontext
+	import wrapper_processing as wp
 	from terapix.youpi.models import *
 	from terapix.lib.common import get_pixel_scale
 	#
@@ -37,68 +40,64 @@ except ImportError, e:
 	print 'Please run this command from the terapix subdirectory.'
 	sys.exit(1)
 
-def main():
-	cur = connection.cursor()
-	images = Image.objects.filter(pixelscale = None)
-	print "Images to process: %d" % len(images)
-	k = 0
-	for im in images:
-		absimgpath = os.path.join(im.path, im.filename + '.fits')
-		im.pixelscale = str(get_pixel_scale(absimgpath))
-		im.save()
-		sys.stdout.flush()
-		k += 1
-		print k
+CLUSTER_OUTPUT_PATH = None
+VERBOSE = False
+done = 0
 
-#			q = """
-#					SELECT psffwhm, psffwhmmin, psffwhmmax FROM youpi_image AS i 
-#					INNER JOIN youpi_rel_it AS it ON i.id = it.image_id 
-#					INNER JOIN youpi_processing_task AS t ON t.id = it.task_id 
-#					INNER JOIN youpi_plugin_fitsin AS f ON f.task_id = t.id 
-#					WHERE i.name=\'%s\';
-#		
-#				""" % (im.name)
-#
-#			cur.execute(q)
-#			res = cur.fetchall()
-#
-#			if res:
-#				print "pixel scale mean for %s : %s, with current seeing in pixel : %s, right seeing average value : %s" % (absimgpath, pixelscale,res[0][0],pixelscale*float(res[0][0]))
-#				u = """
-#						UPDATE youpi_plugin_fitsin 
-#						INNER JOIN youpi_processing_task ON youpi_plugin_fitsin.task_id = youpi_processing_task.id 
-#						INNER JOIN youpi_rel_it ON youpi_processing_task.id = youpi_rel_it.task_id 
-#						INNER JOIN youpi_image ON youpi_rel_it.image_id = youpi_image.id 
-#						SET psffwhm = \'%s\', psffwhmmin = \'%s\', psffwhmmax = \'%s\' 
-#						WHERE youpi_image.name=\'%s\';
-#				""" % (round(pixelscale*float(res[0][0]), 8), round(pixelscale*float(res[0][1]),8), round(pixelscale*float(res[0][2]),8), im.name)
-#				
-#				cur.execute(u)
-#				connection._commit()
-#			else:
-#				print "No seeing already updated in data base for image %s" % absimgpath
-	connection.close()
+def main(g):
+	global done
+	images = Image.objects.filter(pixelscale = None)
+	only_img = False
+	if only_img:
+		print "Images to process: %d" % len(images)
+		k = 0
+		for im in images:
+			absimgpath = os.path.join(im.path, im.filename + '.fits')
+			im.pixelscale = str(get_pixel_scale(absimgpath))
+			im.save()
+			sys.stdout.flush()
+			k += 1
+			print k
+
+	q = """
+SELECT f.id, t.results_output_dir, t.id FROM youpi_processing_task AS t, youpi_plugin_fitsin AS f
+WHERE f.task_id=t.id
+AND t.success=1
+"""
+	res = g.execute(q)
+	total = len(res)
+	print "Number of QFits to process: %d" % total
+
+	for r in res:
+		image_id = g.execute("SELECT image_id FROM youpi_rel_it WHERE task_id=%d" % r[2])[0][0]
+		image = Image.objects.filter(id = image_id)[0]
+		userData = {'RealImageName': image.filename}
+		vout = " QFits %06d/%d [T: %d F:%d I:%s]" % (done+1, total, r[2], r[0], userData['RealImageName'])
+		if VERBOSE:
+			print '-' * 2 + vout + '-' * 30
+			print log
+		else:
+			sys.stdout.write('.')
+			sys.stdout.flush()
+		try:
+			log =  wp.ingestQFitsInResults(r[1], userData, r[0], g)
+		except Exception, e:
+			print "\nError at%s\nException: %s" % (vout, e)
+		g.con.commit()
+		done += 1
 
 if __name__ == '__main__':
-	#main()
-	import wrapper_processing as wp
-	psfexFieldNames = (
-		'FWHM_Min', 'FWHM_Mean', 'FWHM_Max', 'Elongation_Min', 'Elongation_Mean', 
-		'Elongation_Max', 'Chi2_Min', 'Chi2_Mean', 'Chi2_Max', 'Residuals_Min',
-		'Residuals_Mean', 'Residuals_Max', 'Asymetry_Min', 'Asymetry_Mean',
-		'Asymetry_Max', 'NStars_Accepted_Min', 'NStars_Accepted_Mean', 
-		'NStars_Accepted_Max', 
-	)
-#	print wp.parse_psfex_xml('/tmp/psfex.xml', psfexFieldNames)
-
 	from DBGeneric import DB, DBGeneric
 	db = DB(host = settings.DATABASE_HOST,
 			user = settings.DATABASE_USER,
 			passwd = settings.DATABASE_PASSWORD,
 			db = settings.DATABASE_NAME)
 	g = DBGeneric(db.con)
-	wp.ingestQFitsInResults(35824, g)
-	g.con.commit()
+	start = time.time()
+	try:
+		main(g)
+	except KeyboardInterrupt:
+		pass
+	print "\nTotal: processed %d qfits in %.2f seconds." % (done, time.time() - start)
+
 	db.close()
-
-
