760dca516c0e90371342f221225300c74c57c237
Mathias Monnerville <monnerville@iap.fr>
2011-02-09 14:53:36 +0100

    Removed copyright info from source headers
    Instead, added license info to the LICENSE file.

--- terapix/youpi/plugins/qualityfitsin.py
+++ terapix/youpi/plugins/qualityfitsin.py
@@ -1,16 +1,3 @@
-##############################################################################
-#
-# Copyright (c) 2008-2009 Terapix Youpi development team. All Rights Reserved.
-#                    Mathias Monnerville <monnerville@iap.fr>
-#                    Gregory Semah <semah@iap.fr>
-#
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
-#
-##############################################################################
-
 # vim: set ts=4
 
 import sys, os.path, re, time, string, re
